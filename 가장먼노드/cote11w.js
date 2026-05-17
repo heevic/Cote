eddd8224add167160adf682c8951743710b7dd97
heevic <dbgmltmd0317@gmail.com>
2026-05-17 20:38:17 +0900

    init

--- 가장먼노드/cote11w.js
+++ 가장먼노드/cote11w.js
@@ -1,26 +1,37 @@
 function solution(n, vertex) {
-    const graph = Array.from({ length: n + 1 }, () => []);
+    // 그래프
+    const neighbors = Array.from({ length: n + 1 }, () => []);
 
     for (const [a, b] of vertex) {
-        graph[a].push(b);
-        graph[b].push(a);
+        neighbors[a].push(b);
+        neighbors[b].push(a); // 양방향
     }
 
-    const distance = Array(n + 1).fill(-1);
-    distance[1] = 0;
-    const queue = [1];
+    // 최단거리 배열
+    const distances = Array(n + 1).fill(-1);
+
+    // BFS
+    const queue = [[1, 0]];
     let front = 0;
 
     while (front < queue.length) {
-        const current = queue[front++];
-        for (const next of graph[current]) {
-            if (distance[next] === -1) {
-                distance[next] = distance[current] + 1;
-                queue.push(next);
-            }
+        const [node, distance] = queue[front++];
+
+        // 이미 방문한 노드면 무시
+        if (distances[node] >= 0) continue;
+
+        // 최단거리 기록
+        distances[node] = distance;
+
+        // 다음 노드 등록
+        for (const next of neighbors[node]) {
+            queue.push([next, distance + 1]);
         }
     }
 
-    const maxDistance = Math.max(...distance);
-    return distance.filter(v => v === maxDistance).length;
+    // 가장 먼 거리
+    const maxDistance = Math.max(...distances);
+
+    // 가장 먼 노드 개수
+    return distances.filter(v => v === maxDistance).length;
 }
